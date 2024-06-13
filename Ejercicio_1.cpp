f10885f4c47b61bfdfbcdbed1779582a8a061f25
Yair292 <dluquep@unjbg.edu.pe>
2024-06-13 12:23:33 -0500

    Actualiza ejercicio 1 de estructuras selectivas: agrega validación de entrada

--- Ejercicio_1.cpp
+++ Ejercicio_1.cpp
@@ -3,15 +3,21 @@
 using namespace std;
 
 int main(){
-	float a,b,c,promedio;
+	float nota1,nota2,nota3,promedio;
 	cout<<"Digita las tres calificaciones: "<<endl;
-	cin>> a >> b >> c;
-	promedio = (a+b+c)/3;
-	if (promedio>=70){
-	    cout<<"Aprobo el curso";	
+	cin>> nota1 >> nota2 >> nota3;
+	promedio = (nota1+nota2+nota3)/3;
+	if (nota1<0 || nota2<0 || nota3<0){
+		cout<<"ERROR.NEGATIVE";
 	}else{
+		if (promedio>=70){
+	    cout<<"Aprobo el curso";
+	    }else{
 		cout<<"Reprobo el curso";
+	    }
 	}
+	
+	
 	return 0;
 	//"Si puedes imaginarlo puedes programarlo"
 }
