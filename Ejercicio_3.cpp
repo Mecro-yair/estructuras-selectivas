cd1318bed13cc487664ef83dc75bd177d2376392
Yair292 <dluquep@unjbg.edu.pe>
2024-06-15 20:57:59 -0500

    Se actualizo el Ejercicio_3 de repetitivas:SOLUCION A CASOS NEGATIVOS

--- Ejercicio_3.cpp
+++ Ejercicio_3.cpp
@@ -3,18 +3,19 @@ using namespace std;
 int main(){
 	int numero_de_camisas;
 	float total_compra,total_a_pagar;
-	cout<<"Digite el numero de camisas"<<endl;
-	cin>>numero_de_camisas;
-	cout<<"Digite el costo total de las camisas"<<endl;
-	cin>>total_compra;
-	
-	if (numero_de_camisas>=3){
+	cout<<"Digite el numero de camisas:"<<endl; cin>>numero_de_camisas;
+	cout<<"Digite el costo total de las camisas:"<<endl; cin>>total_compra;
+	if (numero_de_camisas>0 && total_compra>0){
+		if (numero_de_camisas>=3){
 		total_a_pagar=total_compra*0.8;
-	}else{
+	    }else{
 		total_a_pagar=total_compra*0.9;
-	}
+	    }
+	    cout<<"Usted debe pagar: "<<total_a_pagar<<endl;
+	}else{
+		cout<<"ERROR3_NEGATIVE_VALUE.\nEnter a positive number.\n";
+	}	
 	
-	cout<<"Usted debe pagar: "<<total_a_pagar<<endl;
 	return 0;
 	//"Si puedes imaginarlo puedes programarlo"
 }
