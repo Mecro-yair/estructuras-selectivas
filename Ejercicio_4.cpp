d810e613ad697e7b88167c20a2612e5a86f6ea06
Yair292 <dluquep@unjbg.edu.pe>
2024-06-16 17:43:55 -0500

    Se añadio una funcion al Ejercicio_4 de selectivas

--- Ejercicio_4.cpp
+++ Ejercicio_4.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
+float descontado(int x,float y);
+float descontado(int x,float y){
+	float dinero_descontado;
+	if (x<74){
+		dinero_descontado=y*0.15;
+	}else{
+		dinero_descontado=y*0.20;
+	}
+    return dinero_descontado;
+}
 int main(){
+	float total_de_compra;
 	int numero_aleatorio;
-	float total_de_compra,dinero_descontado;
 	cout<<"Digite el numero aleatorio que le toco: "<<endl;
 	cin>>numero_aleatorio;
 	cout<<"Digite el total de la compra: "<<endl;
-	cin>>total_de_compra;
-	
-	if (numero_aleatorio<74){
-		dinero_descontado=total_de_compra*0.15;
-	}else{
-		dinero_descontado=total_de_compra*0.20;
-	}
-	
-	cout<<"El dinero que se descontará sera "<<dinero_descontado<<endl;
+	cin>>total_de_compra;		
+	cout<<"El dinero que se descontará sera "<<descontado(numero_aleatorio,total_de_compra)<<endl;
 	
 	return 0;
 	//"Si puedes imaginarlo puedes programarlo"
