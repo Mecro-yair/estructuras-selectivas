2565527b578b13c766f10208aef08508a77ec372
Yair292 <dluquep@unjbg.edu.pe>
2024-06-15 19:20:42 -0500

    Se Actualizo Errores en el Ejercicio_1

--- Ejercicio_1.cpp
+++ Ejercicio_1.cpp
@@ -8,7 +8,7 @@ int main(){
 	cin>> nota1 >> nota2 >> nota3;
 	promedio = (nota1+nota2+nota3)/3;
 	if (nota1<0 || nota2<0 || nota3<0){
-		cout<<"ERROR.NEGATIVE";
+		cout<<"ERROR3_NEGATIVE_VALUE";
 	}else{
 		if (promedio>=70){
 	    cout<<"Aprobo el curso";
