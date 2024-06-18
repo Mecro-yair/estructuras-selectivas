5ced174145e220eac6e358053af68456dd7a2954
Yair292 <dluquep@unjbg.edu.pe>
2024-06-17 22:05:47 -0500

    Se añadio una funcion y se soluciono errores en variables

--- Ejercicio_5.cpp
+++ Ejercicio_5.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 using namespace std;
 
+float numPulsaciones(float x,char y);
+float numPulsaciones(float x,char y){
+	float z;
+    if (y == 'f') {
+    z = (220-x)/10;
+    } else if (y == 'm') {
+    z = (210-x)/10;
+    }
+    return z;
+}
+
 int main() {
     int edad;
     char sexo;
-    float numPulsaciones;
     
-    cout << "Digite su edad: "<<endl;
-    cin >> edad;
-    cout << "Digite su sexo ( m para masculino, f para femenino): "<<endl;
-    cin >> sexo;
-
-    
-    if (sexo == 'f') {
-        numPulsaciones = (220-edad)/10;
-        cout << "El numero de pulsaciones por cada 10 segundos es : " << numPulsaciones << endl;
-    } else if (sexo == 'm') {
-        numPulsaciones = (210-edad)/10;
-        cout << "El numero de pulsaciones por cada 10 segundos es : " << numPulsaciones << endl;
-    } else {
-        cout << "Sexo invalido. Digite 'f' o 'm'." << endl;
-    }
+    cout << "Digite su edad: "<<endl; cin >> edad;
+    cout << "Digite su sexo ( m para masculino, f para femenino): "<<endl; cin >> sexo;
+    if (edad>0 && (sexo=='f'|| sexo=='m')){
+    	cout << "El numero de pulsaciones por cada 10 segundos es : " <<numPulsaciones(edad,sexo)<< endl;
+	}else if(edad<=0){
+		cout<<"Digite una edad valida";
+    }else{
+    	cout << "Sexo invalido. Digite 'f' o 'm'." << endl;
+	}
     return 0;
     //"Si puedes imaginarlo puedes programarlo"
 }
