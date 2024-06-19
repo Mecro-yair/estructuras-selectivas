ac6b70c6d7d17ac8b7314d5442640b3843dff30e
Yair292 <dluquep@unjbg.edu.pe>
2024-06-18 22:34:33 -0500

    Se agrego una funcion y se soluciono para casos negativos

--- Ejercicio_6.cpp
+++ Ejercicio_6.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
-int main() {
-    float promedio,pension,igv,total;
-    cout << "Digite el promedio obtenido en el ultimo periodo: "<<endl;
-    cin >> promedio;
-    cout << "Digite la pension educativa : "<<endl;
-    cin >> pension;
+//Declaro la funcion
+float total(float x,float y);
 
-    if (promedio >= 18) {
-        pension = pension*0.7;
+float total(float x,float y){
+	float igv;
+	float total;
+	//si promedio es mayor o igual a 18 solo paga el 70%
+	if (x >= 18) {
+        y = y*0.7;
         igv = 0;
     } else {
-        igv = 0.18 * pension;
+        igv = 0.18 * y; //si no se le cobra el igv
     }
-    total = pension + igv;
+    total = y + igv;    // aqui se le cobra
+    return total;
+}
+
+
+int main() {
+    float promedio,pension;
+    cout << "Digite el promedio obtenido en el ultimo periodo: "<<endl;cin >> promedio;
+    cout << "Digite la pension educativa : "<<endl;cin >> pension;  
+    if (promedio>=0 && pension>=0){
+    	cout << "El alumno debe pagar: "<< total(promedio,pension) << endl;
+	}else{
+		cout<<"ERROR.Digito un numero negativo";
+	} 
     
-    
-    cout << "El alumno debe pagar: "<< total << endl;
     return 0;
     //"Si puedes imaginarlo puedes programarlo"
 }
